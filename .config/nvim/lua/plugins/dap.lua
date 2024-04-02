eacb1d7481ce6e6e47ab64046e5f69fe9dffe658
Andy Miller <amcoder@gmail.com>
2024-04-01 23:10:07 -0400

    Update neovim

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -5,6 +5,7 @@ return {
       -- Creates a beautiful debugger UI
       'rcarriga/nvim-dap-ui',
       'theHamsta/nvim-dap-virtual-text',
+      'nvim-neotest/nvim-nio',
 
       -- Installs the debug adapters for you
       'williamboman/mason.nvim',
