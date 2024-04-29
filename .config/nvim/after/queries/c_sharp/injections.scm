1e54b0a083132d2f1ba2db093311a254522896ab
Andy Miller <amcoder@gmail.com>
2024-04-28 20:18:57 -0400

    Add c# regex injections

--- .config/nvim/after/queries/c_sharp/injections.scm
+++ .config/nvim/after/queries/c_sharp/injections.scm
@@ -1,5 +1,36 @@
 ; extends
 ((raw_string_literal) @injection.content
-  (#match? @injection.content "^\"\"\"\n\\s*\\<([a-zA-Z0-9]+|\\?xml)")
+  (#match? @injection.content "^\"\"\"\n?\\s*\\<([a-zA-Z0-9]+|\\?xml)")
   (#offset! @injection.content 0 3 0 -3)
   (#set! injection.language "xml"))
+
+(object_creation_expression
+    type: (identifier) @type (#eq? @type "Regex")
+    arguments: (argument_list
+      .
+      (argument
+        .
+          (verbatim_string_literal) @injection.content
+          (#offset! @injection.content 0 2 0 -1)
+          (#set! injection.language "regex"))))
+
+(attribute
+  name: (identifier) @attribute (#eq? @attribute "GeneratedRegex")
+  (attribute_argument_list
+    .
+    (attribute_argument
+      .
+        (verbatim_string_literal) @injection.content
+        (#offset! @injection.content 0 2 0 -1)
+        (#set! injection.language "regex"))))
+
+(invocation_expression
+  function: (member_access_expression
+    name: (identifier) @function (#eq? @function "Match"))
+  arguments: (argument_list
+    (argument)
+    (argument
+      .
+        (verbatim_string_literal) @injection.content
+        (#offset! @injection.content 0 2 0 -1)
+        (#set! injection.language "regex"))))
