ed61b9e956d4a338fd0705b15468d887f499058d
Andy Miller <amcoder@gmail.com>
2024-05-01 02:56:04 -0400

    Add oil.nvim plugin

--- .config/nvim/lua/plugins/utils.lua
+++ .config/nvim/lua/plugins/utils.lua
@@ -60,4 +60,13 @@ return {
       })
     end,
   },
+
+  {
+    'stevearc/oil.nvim',
+    -- Optional dependencies
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = {
+      default_file_explorer = false,
+    },
+  },
 }
