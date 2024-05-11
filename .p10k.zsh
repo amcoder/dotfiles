3c75d84800f05232cc3b11dce1eceddeb78736d9
Andy Miller <amcoder@gmail.com>
2024-05-11 01:35:21 -0400

    Remove time from shell prompt

--- .p10k.zsh
+++ .p10k.zsh
@@ -98,7 +98,7 @@
     todo                    # todo items (https://github.com/todotxt/todo.txt-cli)
     timewarrior             # timewarrior tracking status (https://timewarrior.net/)
     taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
-    time                    # current time
+    # time                    # current time
     # battery                 # internal battery
     # =========================[ Line #2 ]=========================
     newline
