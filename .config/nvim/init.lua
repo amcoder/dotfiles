707a3aa5c10b4c30f432666fc3ab8e3987388d6a
Andy Miller <amcoder@gmail.com>
2024-05-26 01:47:33 -0400

    Add DAP signs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -72,6 +72,12 @@ vim.fn.sign_define('DiagnosticSignError', { text = ' ', texthl = 'DiagnosticS
 vim.fn.sign_define('DiagnosticSignWarn', { text = ' ', texthl = 'DiagnosticSignWarn' })
 vim.fn.sign_define('DiagnosticSignInfo', { text = ' ', texthl = 'DiagnosticSignInfo' })
 vim.fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticSignHint' })
+vim.fn.sign_define('DapBreakpoint', { text = '●', texthl = 'DapBreakpoint', linehl = '', numhl = '' })
+vim.fn.sign_define(
+  'DapBreakpointCondition',
+  { text = '●', texthl = 'DapBreakpointCondition', linehl = '', numhl = '' }
+)
+vim.fn.sign_define('DapLogPoint', { text = '◆', texthl = 'DapLogPoint', linehl = '', numhl = '' })
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
