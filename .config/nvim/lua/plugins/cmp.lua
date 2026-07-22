86dbf02f1db0a60e73a0160d03de033576d35280
Andy Miller <amcoder@gmail.com>
2026-07-22 14:20:36 -0400

    Switch to blink-cmp

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -1,152 +1,24 @@
 return {
-  { -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-      -- Snippet Engine & its associated nvim-cmp source
-      {
-        'L3MON4D3/LuaSnip',
-        build = (function()
-          -- Build Step is needed for regex support in snippets
-          -- This step is not supported in many windows environments
-          -- Remove the below condition to re-enable on windows
-          if vim.fn.has('win32') == 1 or vim.fn.executable('make') == 0 then
-            return
-          end
-          return 'make install_jsregexp'
-        end)(),
-        dependencies = { 'rafamadriz/friendly-snippets' },
-      },
-      'saadparwaiz1/cmp_luasnip',
+  'saghen/blink.cmp',
+  dependencies = {
+    'saghen/blink.lib',
+    'rafamadriz/friendly-snippets',
+  },
+  build = function()
+    -- build the fuzzy matcher, optionally add a timeout to `pwait(timeout_ms)`
+    -- you can use `gb` in `:Lazy` to rebuild the plugin as needed
+    require('blink.cmp').build():pwait()
+  end,
 
-      -- Adds other completion capabilities.
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-cmdline',
-      'hrsh7th/cmp-emoji',
-      'petertriho/cmp-git',
-
-      -- Show lsp icons in the completion menu
-      'onsails/lspkind.nvim',
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    completion = {
+      documentation = { auto_show = true },
+      list = { selection = { auto_insert = false } },
+      accept = { auto_brackets = { enabled = false } },
     },
-    keys = {
-      {
-        '<leader>C',
-        function()
-          vim.b.disable_cmp = not vim.b.disable_cmp
-          print('Completion ' .. (vim.b.disable_cmp and 'disabled' or 'enabled'))
-        end,
-        mode = '',
-        desc = 'Toggle Completion',
-      },
-    },
-    config = function()
-      -- See `:help cmp`
-      local cmp = require('cmp')
-      local luasnip = require('luasnip')
-      local lspkind = require('lspkind')
 
-      luasnip.config.setup({})
-
-      require('luasnip.loaders.from_vscode').lazy_load()
-
-      cmp.setup({
-        enabled = function()
-          return vim.api.nvim_get_mode().mode == 'c' or not vim.b.disable_cmp
-        end,
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        completion = { completeopt = 'menu,menuone,noselect,noinsert' },
-        formatting = {
-          format = lspkind.cmp_format({
-            mode = 'symbol_text',
-            maxwidth = 50,
-            ellipsis_char = '…',
-            menu = {
-              buffer = '[buf]',
-              nvim_lsp = '[lsp]',
-              luasnip = '[snip]',
-              path = '[path]',
-              cmdline = '[cmd]',
-              git = '[git]',
-            },
-          }),
-        },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-        -- For an understanding of why these mappings were
-        -- chosen, you will need to read `:help ins-completion`
-        --
-        -- No, but seriously. Please read `:help ins-completion`, it is really good!
-        mapping = cmp.mapping.preset.insert({
-          ['<C-n>'] = cmp.mapping.select_next_item(),
-          ['<C-p>'] = cmp.mapping.select_prev_item(),
-
-          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-
-          ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-
-          ['<C-e>'] = cmp.mapping.close(),
-          ['<C-Space>'] = cmp.mapping.complete({}),
-
-          ['<C-l>'] = cmp.mapping(function()
-            if luasnip.expand_or_locally_jumpable() then
-              luasnip.expand_or_jump()
-            end
-          end, { 'i', 's' }),
-          ['<C-h>'] = cmp.mapping(function()
-            if luasnip.locally_jumpable(-1) then
-              luasnip.jump(-1)
-            end
-          end, { 'i', 's' }),
-        }),
-        sources = {
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-          { name = 'buffer' },
-          { name = 'emoji' },
-          { name = 'path' },
-        },
-      })
-
-      -- `/` cmdline setup.
-      cmp.setup.cmdline({ '/', '?' }, {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = 'buffer' },
-        },
-      })
-
-      -- `:` cmdline setup.
-      cmp.setup.cmdline(':', {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = 'path' },
-        }, {
-          {
-            name = 'cmdline',
-            option = {
-              ignore_cmds = { 'Man', '!', 'GoTest', 'GoTestFunc', 'GoTestFile', 'GoTestPkg', 'GoTestSum' },
-            },
-          },
-        }),
-      })
-
-      cmp.setup.filetype('gitcommit', {
-        sources = cmp.config.sources({
-          { name = 'git' },
-        }, {
-          { name = 'emoji' },
-        }),
-      })
-
-      require('cmp_git').setup()
-    end,
+    signature = { enabled = true },
   },
 }
